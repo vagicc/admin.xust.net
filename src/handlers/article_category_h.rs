9d55ed02538c5e6ae05889efeae16cf6423330d9
临来笑笑生 <luck@xust.net>
2024-08-20 20:46:57 +0800

    文章分类－新增

--- src/handlers/article_category_h.rs
+++ src/handlers/article_category_h.rs
@@ -1,7 +1,8 @@
 use crate::models::article_category_m;
 use crate::session::Session;
+use crate::template::to_html_single;
 use crate::template::view;
-use handlebars::{to_json};
+use handlebars::to_json;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::value::Map;
 use warp::{Rejection, Reply};
@@ -28,5 +29,82 @@ pub async fn list_page(
 
     let html = view("article-category/list.html", data, session);
 
+    Ok(warp::reply::html(html)) //直接返回html
+}
+
+/* 响应GET： /article-category/create */
+pub async fn create(session: Session) -> std::result::Result<impl Reply, Rejection> {
+    log::debug!("[调试信息]访问了“/article-category/create/”");
+
+    let mut data = Map::new();
+    // let html = to_html_single("reptile_new.html", data);
+    let html = view("article-category/create.html", data, session);
+
+    Ok(warp::reply::html(html)) //直接返回html
+                                // Err(warp::reject::not_found())   //错误的返回状态码
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct ArticleCategoryPost {
+    pub category: String,
+    pub order_by: i16,
+    pub show: i16, ////是否显示：默认1显示，0不显示
+    pub seo_title: String,
+    pub seo_keywords: String,
+    pub seo_description: String,
+}
+impl ArticleCategoryPost {
+    pub fn validate(&self) -> Result<Self, &'static str> {
+        if self.category.is_empty() {
+            return Err("分类名不能为空");
+        }
+
+        if self.show > 1 {
+            return Err("是否显示：默认1显示，0不显示");
+        }
+
+        Ok(self.clone())
+    }
+}
+
+pub async fn new(
+    form: ArticleCategoryPost,
+    session: Session,
+) -> std::result::Result<impl Reply, Rejection> {
+    log::debug!("接收到的post:{:#?}", form);
+    let mut message = String::new();
+
+    match form.validate() {
+        Ok(post) => {
+            let now = crate::common::now_naive_date_time();
+
+            let data = article_category_m::NewArticleCategory {
+                category: post.category,
+                seo_title: Some(post.seo_title),
+                seo_keywords: Some(post.seo_keywords),
+                seo_description: Some(post.seo_description),
+                show: post.show, //是否显示：默认1显示，0不显示
+                order_by: Some(post.order_by),
+                modify_id: None,
+                modify_time: None,
+                create_id: Some(session.admin.id),
+                create_time: Some(now),
+            };
+            if data.insert() != 0 {
+                message = "新增文章分类成功！".to_string();
+            } else {
+                message = "插入文章分类失败".to_string();
+            }
+        }
+        Err(e) => {
+            message = format!("POST表单认证不通过：{}", e);
+        }
+    }
+
+    let mut data = Map::new();
+    data.insert("jump_url".to_string(), to_json("/article-category/index"));
+    data.insert("message".to_string(), to_json(message));
+
+    let html = to_html_single("hint.html", data);
     Ok(warp::reply::html(html)) //直接返回html
 }
--- src/routes/article_category_r.rs
+++ src/routes/article_category_r.rs
@@ -5,6 +5,20 @@ use warp::Filter;
 /// GET: /article-category/index/{1}
 pub fn index() -> impl warp::Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone
 {
+    let new = warp::get()
+        .and(warp::path("article-category"))
+        .and(warp::path("create"))
+        .and(warp::path::end())
+        .and(with_session())
+        .and_then(article_category_h::create)
+        .or(warp::post()
+            .and(warp::path("article-category"))
+            .and(warp::path("create"))
+            .and(warp::path::end())
+            .and(warp::body::form())
+            .and(with_session())
+            .and_then(article_category_h::new));
+
     warp::get()
         .and(warp::path("article-category"))
         .and(warp::path("index"))
@@ -20,4 +34,5 @@ pub fn index() -> impl warp::Filter<Extract = (impl warp::Reply,), Error = warp:
             .and(warp::path::end())
             .and(with_session())
             .and_then(article_category_h::list_page))
+        .or(new)
 }
